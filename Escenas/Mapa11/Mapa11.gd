1e97e82376582c2dd653f357a7f443bace483c87
Leonardo Maidana <leo-lomas1@hotmail.com>
2020-06-03 16:58:42 -0300

    Barra_energia_func
    Esta instanciada en todas las escenas la barra de energia con la configuracion optima y tambien en la escena de alimentacion.

--- Escenas/Mapa11/Mapa11.gd
+++ Escenas/Mapa11/Mapa11.gd
@@ -4,7 +4,6 @@ var a = Vector2(100,100)
 
 func _ready():
 	$Personaje.position = Global.posicion_personaje 
-	
 func _on_Mapa12_body_entered(body):
 	Global.posicion_personaje = Vector2(100,200)
 	get_tree().change_scene("res://Escenas/Mapa12/Mapa12.tscn")
@@ -13,4 +12,5 @@ func _on_Mapa12_body_entered(body):
 func _on_Mapa13_body_entered(body):
 	Global.posicion_personaje = Vector2(151,400)
 	$Personaje.position = a
-	pass # Replace with function body.
+	pass
+
