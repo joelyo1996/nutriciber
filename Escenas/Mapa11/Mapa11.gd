62145aa955e8d473efabdfe4452a99b8f4610a33
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2020-06-12 08:34:17 -0300

    cambio
    cambio de escenas

--- Escenas/Mapa11/Mapa11.gd
+++ Escenas/Mapa11/Mapa11.gd
@@ -14,5 +14,5 @@ func _on_Mapa12_area_entered(area):
 	if area.name == "AreaPersonaje":
 		Global.Cambio = 1
 		Global.posicion_personaje = Vector2(100,200)
-		#get_tree().change_scene(mapa12)
+		get_tree().change_scene(mapa12)
 	pass 
